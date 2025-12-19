ee03c54425075c89cce31b930606257b63eb3765
AreLory <arena.lorenzo2406@gmail.com>
2025-12-19 15:46:32 +0100

    Cleaning repo

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -14,10 +14,10 @@ export default function NotFound() {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="flex flex-col justify-center items-center  border-quaternary border-x-2 h-[50vh] w-[50vw] rounded-xl p-8">
-        <h2 className="text-9xl text-quaternary font-extrabold">404</h2>
-        <p className="text-mywhite text-5xl p-4">Page not found</p>
-        <p className="text-mywhite text-3xl p-4">Go back home page and try again</p>
+      <div className="flex flex-col justify-center items-center  border-quaternary border-x-2 h-[50vh] w-[90vw]  rounded-xl p-8">
+        <h2 className="text-5xl md:text-9xl text-quaternary font-extrabold">404</h2>
+        <p className="text-mywhite text-2xl md:text-5xl p-4">Page not found</p>
+        <p className="text-mywhite text-xl md:text-3xl p-4">Go back home page and try again</p>
         <a className="bg-secondary p-4 rounded-xl text-mywhite hover:scale-110 transition hover:bg-primary" 
         type="button" href="/">
         
